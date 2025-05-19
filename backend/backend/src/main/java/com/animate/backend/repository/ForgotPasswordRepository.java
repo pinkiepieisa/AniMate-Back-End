24297c3e1e8b408ed4631bcd4d5c7c9a12b78c68
Bela <imcdsls10@gmail.com>
2025-05-19 16:21:40 -0300

    Reinício...

--- backend/backend/src/main/java/com/animate/backend/repository/ForgotPasswordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.animate.backend.repository;
-
-import com.animate.backend.entities.ForgotPassword;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer> {
-}
