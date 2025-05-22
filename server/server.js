36f837cf624b1d2ea21a42e989ab6b4c35474ff9
Bela <imcdsls10@gmail.com>
2025-05-22 11:32:13 -0300

    Server Socket.io

---
+++ server/server.js
@@ -0,0 +1,39 @@
+//importação das bibliotecas
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+//Permite a conexão externa
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        //Aceita as conexões de qualquer origem
+        methods: ["GET", "POST"]
+        }
+});
+
+//Usuário conectado
+io.on('connection', (socket) => {
+    console.log('Usuário conectado!');
+
+    //Para a conexão com a canvas
+    socket.on('draw', (data) => {
+        socket.broadcast.emit('draw', data);
+    });
+
+    //Usuário desconectado
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado!');
+    });
+});
+
+//HTTP SERVER
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`);
+});
