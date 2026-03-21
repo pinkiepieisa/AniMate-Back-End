cab5f6cad4286af620856db89c67039cdb043fe4
leandro <l34ndr0penha@gmail.com>
2026-03-20 23:50:12 -0300

    adiciona funcionalidade lembrar-de-mim no login

---
+++ backend/backend/src/main/java/com/animate/backend/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.animate.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+
+    private String email;
+    private String password;
+    private boolean rememberMe;
+
+}
